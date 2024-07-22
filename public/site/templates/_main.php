f9d8b0976952767dee9b7ba3a5b33554816411e5
keanedalisay <keandals21@gmail.com>
2024-07-22 09:48:24 +0800

    Fix viewport metadata not included

--- public/site/templates/_main.php
+++ public/site/templates/_main.php
@@ -22,6 +22,7 @@ $home = $pages->get('/'); /** @var HomePage $home */
 
 <head id="head">
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title><?php echo $page->title; ?></title>
 	<link rel="stylesheet" type="text/css" href="<?= $config->urls->templates; ?>styles/global.css" />
 	<script src="<?= $config->urls->templates; ?>scripts/main.js" defer></script>
