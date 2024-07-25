0356574772e86fbb8aac578ea2238a72e9a65bbb
keanedalisay <keandals21@gmail.com>
2024-07-25 17:12:26 +0800

    Add service pages to ProcessWire

---
+++ public/site/templates/services-volunteer.php
@@ -0,0 +1,31 @@
+<?php
+namespace ProcessWire;
+
+/**
+ * @var Page $page
+ * @var Pages $pages
+ * @var Config $config
+ * 
+ */
+?>
+
+<head id="head" pw-append>
+	<link rel="stylesheet" href="<?= $config->urls->templates ?>/styles/services.css">
+</head>
+
+<main id="content" pw-prepend>
+    <div class="services-container">
+        <div class="title"> 
+            Volunteer Opportunities
+        </div>
+        <div class="services-about">
+            Donation drives, community service,and other philantrophic work.
+        </div>
+
+        <div class="volunteer-oppurtunities">
+            <div class="opportunities-contents">
+                No volunteer opportunities are open hence this section is empty. Come back at another time.
+            </div>
+        </div>
+    </div>
+</main>
