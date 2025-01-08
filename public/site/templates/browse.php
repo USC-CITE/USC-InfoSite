fd1cd40436106fedd4ddf18f353050aad4bc2436
Keane Emanuel Dalisay <keandals21@gmail.com>
2025-01-08 13:30:22 +0800

    Fix link references on browse page

--- public/site/templates/browse.php
+++ public/site/templates/browse.php
@@ -19,20 +19,20 @@ namespace ProcessWire;
 		<div class="main__information">
 			<h3 class="main__heading">Information </h3>
 			<div class="main__links">
-				<a href="#" class="main__items">Announcements</a>
-				<a href="#" class="main__items">Events</a>
-				<a href="#" class="main__items">Organizations</a>
-				<a href="#" class="main__items">About the USC</a>
-				<a href="#" class="main__items">Officers of the USC</a>
+				<a href="/announcements" class="main__items">Announcements</a>
+				<a href="/events" class="main__items">Events</a>
+				<a href="/organizations" class="main__items">Organizations</a>
+				<a href="/usc" class="main__items">About the USC</a>
+				<a href="/usc/officers" class="main__items">Officers of the USC</a>
 			</div>
 		</div>
 		<div class="main__services">
 			<h3 class="main__heading">Services </h3>
 			<div class="main__links">
-				<a href="#" class="main__items">Volunteer Opportunities</a>
-				<a href="#" class="main__items">Education and Training</a>
-				<a href="" class="main__items">Contact the USC</a>
-				<a href="" class="main__items">Contact the USC-CITE</a>
+				<a href="/services/volunteer" class="main__items">Volunteer Opportunities</a>
+				<a href="/services/education" class="main__items">Education and Training</a>
+				<a href="/usc/contact" class="main__items">Contact the USC</a>
+				<a href="/usc/cite" class="main__items">Contact the USC-CITE</a>
 			</div>
 		</div>
 	</div>
