263f12f8c6a568e46c54368ab9b9f92bef9b1c42
Keane Emanuel Dalisay <keandals21@gmail.com>
2025-01-09 10:53:02 +0800

    Add Google Maps embed for direction to office of USC

--- public/site/templates/usc-contact.php
+++ public/site/templates/usc-contact.php
@@ -20,7 +20,7 @@ namespace ProcessWire;
 			<h1 class="main__heading">Contact the University Student Council</h1>
 			<p class="main__text">If you are looking to question, create partnerships, or propose advocacies with us,
 				don’t hesitate to email at
-				<a href="mailto:<?= $page->org_email ?>"><?= $page->org_email ?></a>
+				<a href="mailto:<?= $page->org_email ?>"><?= $page->org_email ?></a>.
 			</p>
 
 			<div class="main__container main__container--contact-details main__container--contact-details--mobile">
@@ -44,7 +44,7 @@ namespace ProcessWire;
 			<p class="main__details">West Visayas State University</p>
 			<p class="main__details">Luna Street, Lapaz, Iloilo City, Iloilo, Philippines</p>
 
-			<div class="main__rectangle"></div>
+			<iframe class="main__rectangle" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d777.882138020866!2d122.5623034793979!3d10.712852728349759!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33aee51661f8773b%3A0xb11ef2ee59e929c8!2sWVSU%20Hometel%2C%20Jose%20Aguilar%20Dr%2C%20La%20Paz%2C%20Iloilo%20City%2C%20Iloilo!5e0!3m2!1sen!2sph!4v1736390706003!5m2!1sen!2sph" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 		</div>
 		<div class="main__container main__container--contact-details">
 			<p>Adviser/s: <br><span><?= $page->org_advisers ?></span></p>
