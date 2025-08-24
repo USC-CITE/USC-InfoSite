29a38badc7238fff75eba87be3df7a758c28521c
tqcss <134121905+tqcss@users.noreply.github.com>
2025-08-24 08:06:56 +0800

    Add individual report display

--- public/site/templates/transparency.php
+++ public/site/templates/transparency.php
@@ -15,39 +15,75 @@ namespace ProcessWire;
 </head>
 
 <main id="content" pw-prepend>
-<div class="text-container">
-    <p class="title">Transparency Reports</p>
-    <p class="desc">Resolutions and financial reports of the University Student Council that are open for access.</p>
-</div>
-<div class="content-container">
-    <?php foreach($page->transparency_category as $category):
-        $category_name = $category->transparency_category_name;
-        $category_icon = $_GET["category->transparency_category_icon_name"] ?? "resolutions-icon.svg";
-        $category_reports = $category->transparency_report;?>
+<?php 
+    $category_id = (int) $input->get->category;
+    $report_id = (int) $input->get->report;
 
-        <div class="category-container">
-            <div class="category-header">
-                <div class="icon-text">
-                    <img class="icon" src="<?= $config->urls->templates ?>/assets/icons/<?= $category_icon ?>" alt="<?= $category_name ?> icon" draggable="false">
-                    <p class="category-text"><?= $category_name ?></p>
-                </div>
-                <div class="arrow">
-                    <img src="<?= $config->urls->templates ?>/assets/icons/arrow-right.svg" alt="arrow-right">
-                </div>
+    if($category_id):
+        $category = $page->transparency_category->get("id=$category_id");
+        $report = $category->transparency_report->get("id=$report_id");
+?>
+    <div class="text-container">
+        <div class="report-header">
+            <div>
+                <p class="title">Transparency Report</p>
+                <p class="desc">
+                    <p class="report-title"><?= $report->transparency_report_title ?></p>
+                    <div class="report-label">
+                        Category: <b><?= $category->transparency_category_name ?></b>
+                    </div>
+                    <div class="report-label">
+                        Date Passed: <b><?= $report->transparency_report_date_passed ?></b>
+                    </div>
+                </p>
             </div>
-            <?php if(count($category_reports)==0): ?>
-                <div class="reports-container no-reports">No reports available</div>
-            <?php else: ?>
-                <ul class="reports-container">
-                    <?php foreach($category_reports as $report): 
-                        $report_title = $report->transparency_report_title;
-                        $report_file = $report->transparency_report_files; ?>
-
-                    <li><a class="reports-link" href="#"><?= $report_title ?></a></li>
-                    <?php endforeach; ?>
-                </ul>
-            <?php endif; ?>
+            <div class="back-link">
+                <img class="back-icon" src="<?= $config->urls->templates ?>/assets/icons/blue-arrow-left.svg" alt="back icon" draggable="false">
+                <a href="<?= $page->url ?>">Back to all reports</a>
+            </div>
         </div>
-    <?php endforeach; ?>
-</div>
+    </div>
+<?php else: ?>
+    <div class="text-container">
+        <p class="title">Transparency Reports</p>
+        <p class="desc">Resolutions and financial reports of the University Student Council that are open for access.</p>
+    </div>
+
+    <div class="content-container">
+        <?php foreach($page->transparency_category as $category):
+            $category_name = $category->transparency_category_name;
+            $category_icon = $_GET["category->transparency_category_icon_name"] ?? "resolutions-icon.svg";
+            $category_reports = $category->transparency_report;?>
+
+            <div class="category-container">
+                <div class="category-header">
+                    <div class="icon-text">
+                        <img class="icon" src="<?= $config->urls->templates ?>/assets/icons/<?= $category_icon ?>" alt="<?= $category_name ?> icon" draggable="false">
+                        <p class="category-text"><?= $category_name ?></p>
+                    </div>
+                    <div class="arrow">
+                        <img src="<?= $config->urls->templates ?>/assets/icons/arrow-right.svg" alt="arrow-right">
+                    </div>
+                </div>
+
+                <?php if(count($category_reports)==0): ?>
+                    <div class="reports-container no-reports">No reports available</div>
+                <?php else: ?>
+                    <ul class="reports-container">
+                        <?php foreach($category_reports as $report): 
+                            $report_title = $report->transparency_report_title;
+                            $report_file = $report->transparency_report_files; ?>
+
+                            <li>
+                                <a class="reports-link" href="<?= $page->url ?>?category=<?= $category->id ?>&report=<?= $report->id ?>">
+                                    <?= $report_title ?>
+                                </a>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+            </div>
+        <?php endforeach; ?>
+    </div>
+<?php endif; ?>
 </main> 
