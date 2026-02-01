85d0da43e43414b896d145949fba1656bfab39a4
Keane Emanuel Dalisay <keandals21@gmail.com>
2026-02-01 17:06:08 +0800

    Refactor design of single day events

--- public/site/templates/events.php
+++ public/site/templates/events.php
@@ -10,9 +10,9 @@ namespace ProcessWire;
 
 $page->of(false);
 
-$events = $pages->find("template=event, sort=event_start_date"); 
+$events = $pages->find("template=event, sort=event_start_date");
 
-foreach($events as $event) {
+foreach ($events as $event) {
   if ($event->archive_event) {
     $event_archive = $pages->get("template=event-archive");
     $event->setParent($event_archive);
@@ -47,80 +47,121 @@ $page->of(true);
   <?php if (count($events) < 1): ?>
     <!-- Missing events -->
     <div class="no-content-placeholder">
-        <h2 class="no-content-placeholder__text">No upcoming events at the moment! Come back at another time.</h2>
+      <h2 class="no-content-placeholder__text">No upcoming events at the moment! Come back at another time.</h2>
     </div>
   <?php else: ?>
     <div class="present-events">
-    <?php 
-    $last_start_date;
-    foreach ($events as $event): ?>
-      <?php if (empty($last_start_date) || $last_start_date !== $event->event_start_date): ?>
-        <?php $last_start_date = $event->event_start_date; ?>
-        <div class="event-card event-card--date">
-          <?= datetime("l, F j, Y", $event->event_start_date); ?>
-          <div class="event-card--border">
-            <div class="event-card-status">
-              <?php  
-              $event_date = new \DateTime($event->event_start_date);
-              $date_now = new \DateTime(date("Y-m-d"));
-              $difference = $event_date->diff($date_now);
-          
-              if ($date_now > $event_date) {
-                echo "Event has <span>already ended<span></span>";
-              }
-              else if ($difference->days > 1) {
-                echo "<span>$difference->days days </span> till event starts";
-              }
-              else if ($difference->days === 1) {
-                echo "Event starts <span>tomorrow</span>";
-              }
-              else if ($difference->days === 0) {
-                echo "Event starts <span>today</span>";
-              }
-              ?>
-            </div>
-            <div class="event-title-card">
-              <a class="event-title" href="<?= $event->url ?>"><?= $event->event_name; ?></a>
-            </div>
-            <div class="event-where-when">
-              <div class="event-location">
-                <img class="location-vector" src="<?= $config->urls->templates ?>assets/icons/location-vector.svg" />
-                <strong>Venue:</strong> <?= $event->event_venues_and_st[0]->event_venue; ?>
+      <?php
+      $last_event_date;
+      $last_event_year;
+      foreach ($events as $event): ?>
+        <?php if (empty($last_event_date) || ($last_event_date !== $event->event_start_date && $last_event_year !== datetime("Y", $event->event_start_date))): ?>
+          <?php
+          $last_event_date = $event->event_start_date;
+          $last_event_year = datetime("Y", $event->event_start_date);
+          ?>
+          <div class="event-card event-card--year">
+            <?= datetime("Y", $event->event_start_date); ?>
+            <div class="event-card--border">
+              <div class="event-status-date">
+                <div class="event-status
+                <?php
+                $event_date = new \DateTime($event->event_start_date);
+                $date_now = new \DateTime(date("Y-m-d"));
+                $difference = $event_date->diff($date_now);
+
+                if (!($date_now > $event_date)) {
+                  if ($difference->days >= 1) {
+                    echo "event-status--starts-on";
+                  } else if ($difference->days === 0) {
+                    echo "event-status--ongoing";
+                  } 
+                }
+                ?>
+                ">
+                  <?php
+                  if ($date_now > $event_date) {
+                    echo "Event concluded";
+                  } else if ($difference->days >= 1) {
+                    echo "Event starts on";
+                  } else if ($difference->days === 0) {
+                    echo "Event ongoing";
+                  }
+                  ?>
+                </div>
+                <div class="event-date">
+                  <?= $event_date->format("F") ?>
+                  <span><?= $event_date->format("j") ?></span>
+                </div>
               </div>
-              <div class="event-start-time">
-                <img class="clock-vector" src="<?= $config->urls->templates ?>assets/icons/clock-vector.svg" />
-                <strong>Start Time:</strong> <?= $event->event_venues_and_st[0]->event_venue_st; ?>
-              </div>
-            </div>
-          </div>
-        </div>
-      <?php else: ?>
-        <div class="event-card">
-          <div class="event-card--borderless">
-            <div class="event-card-status">Event starts<span> today</span></div>
-            <div class="event-title-card">
-              <a class="event-title" href=""><?= $event->event_name; ?></a>
-              <div class="event-date"><?= $event->event_start_date; ?></div>
-            </div>
-            <div class="event-where-when">
-              <div class="event-location">
-                <img class="location-vector" src="<?= $config->urls->templates ?>assets/icons/location-vector.svg" />
-                <strong>Venue:</strong> <?= $event->event_venue_and_st[0]->event_venue; ?>
-              </div>
-              <div class="event-start-time">
-                <img class="clock-vector" src="<?= $config->urls->templates ?>assets/icons/clock-vector.svg" />
-                <strong>Start Time:</strong> <?= $event->event_venue_and_st[0]->event_venue_st; ?>
+              <div class="event-title-card">
+                <a class="event-title" href="<?= $event->url ?>"><?= $event->event_name; ?></a>
+                <div class="event-location">
+                  <img class="location-vector" src="<?= $config->urls->templates ?>assets/icons/location-vector.svg" />
+                  <strong>Venue:</strong> <?= $event->event_venues_and_st[0]->event_venue; ?>
+                </div>
+                <div class="event-start-time">
+                  <img class="clock-vector" src="<?= $config->urls->templates ?>assets/icons/clock-vector.svg" />
+                  <strong>Start Time:</strong> <?= $event->event_venues_and_st[0]->event_venue_st; ?>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      <?php endif; ?>
-    <?php endforeach; ?>
+        <?php else: ?>
+          <div class="event-card">
+            <div class="event-card--borderless">
+              <div class="event-status-date">
+                <div class="event-status
+                <?php
+                $event_date = new \DateTime($event->event_start_date);
+                $date_now = new \DateTime(date("Y-m-d"));
+                $difference = $event_date->diff($date_now);
+
+                if (!($date_now > $event_date)) {
+                  if ($difference->days >= 1) {
+                    echo "event-status--starts-on";
+                  } else if ($difference->days === 0) {
+                    echo "event-status--ongoing";
+                  } 
+                }
+                ?>
+                ">
+                  <?php
+                  if ($date_now > $event_date) {
+                    echo "Event concluded";
+                  } else if ($difference->days >= 1) {
+                    echo "Event starts on";
+                  } else if ($difference->days === 0) {
+                    echo "Event ongoing";
+                  }
+                  ?>
+                </div>
+                <div class="event-date">
+                  <?= $event_date->format("F") ?>
+                  <span><?= $event_date->format("j") ?></span>
+                </div>
+              </div>
+              <div class="event-title-card">
+                <a class="event-title" href="<?= $event->url ?>"><?= $event->event_name; ?></a>
+                <div class="event-location">
+                  <img class="location-vector" src="<?= $config->urls->templates ?>assets/icons/location-vector.svg" />
+                  <strong>Venue:</strong> <?= $event->event_venues_and_st[0]->event_venue; ?>
+                </div>
+                <div class="event-start-time">
+                  <img class="clock-vector" src="<?= $config->urls->templates ?>assets/icons/clock-vector.svg" />
+                  <strong>Start Time:</strong> <?= $event->event_venues_and_st[0]->event_venue_st; ?>
+                </div>
+              </div>
+            </div>
+          </div>
+        <?php endif; ?>
+      <?php endforeach; ?>
     </div>
   <?php endif; ?>
   </div>
 
   <div class="event-archive">
-    <a href="/events/archive">Look at our event archive</a> to see the countless events we have conducted over the years.
+    <a href="/events/archive">Look at our event archive</a> to see the countless events we have conducted over the
+    years.
   </div>
 </main>
