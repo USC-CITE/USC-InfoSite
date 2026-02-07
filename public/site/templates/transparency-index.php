d2b43a33e3c7e6df9b5fb88b54e22f6e1791be42
tqcss <134121905+tqcss@users.noreply.github.com>
2026-02-07 19:10:21 +0800

    Add transparency-index page

---
+++ public/site/templates/transparency-index.php
@@ -0,0 +1,42 @@
+<?php
+namespace ProcessWire;
+
+/**
+ * @var Page $page
+ * @var Pages $pages
+ * @var Config $config
+ *
+ */
+?>
+
+<head id="head" pw-append>
+    <link rel="stylesheet" href="<?= $config->urls->templates ?>styles/transparency/transparency-index.css">
+</head>
+
+<main id="content" pw-prepend>
+    <div class="text-container">
+        <p class="title">Transparency Reports</p>
+        <p class="desc">Resolutions and financial reports of the University Student Council that are open for access.
+        </p>
+    </div>
+
+    <?php if (!$page->transparency_category->count()): ?>
+        <p class="status-unavailable">Content Unavailable</p>
+
+    <?php else: ?>
+        <div class="resolution-container">
+            <?php foreach ($page->transparency_category as $category):
+                $category_name = $category->transparency_category_name;
+                $category_icon = (empty($category->transparency_category_icon)) ? "resolution-icon.svg" : $category->transparency_category_icon; ?>
+
+                <a class="resolution" href="/">
+                    <div class="icon-text">
+                        <img class="icon" src="<?= $config->urls->templates ?>assets/icons/<?= $category_icon ?>" alt="">
+                        <p class="resolution-text"><?= $category_name ?></p>
+                    </div>
+                    <img class="arrow" src="<?= $config->urls->templates ?>assets/icons/arrow-right.svg" alt="">
+                </a>
+            <?php endforeach; ?>
+        </div>
+    <?php endif ?>
+</main>
